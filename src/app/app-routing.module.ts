e075b04f96090c3fae0ab18fab13e7b1115e29d1
Md. Ariful/Tech/Md. Ariful Islam (Email: islam.ariful@robi.com.bd) <islam.ariful@robi.com.bd>
2021-08-19 19:30:28 +0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home-page',
+  },
+  {
+    path: 'home-page',
+    loadChildren: () =>
+      import('./main-page/main-page.module').then((m) => m.MainPageModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
